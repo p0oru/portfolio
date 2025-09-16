ee8fde548e7bae659f4b86560a2066b97dfc5d2e
Puru Singh <73597961+p0oru@users.noreply.github.com>
2025-09-16 01:32:40 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: set this to your repository name for GitHub Pages project sites
   // If your repo is p0oru.github.io, use '/'
-  base: './', // <-- ADD THIS LINE
+  base: '/portfolio/', // <-- ADD THIS LINE
 
 })
